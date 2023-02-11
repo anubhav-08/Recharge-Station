0a4bd51ad4fac39e3741399101c6e1c7ef42e145
Anubhav Tyagi <tyagianubhav80@gmail.com>
2023-02-11 17:04:58 +0530

    create Plan apis

--- Plan/urls.py
+++ Plan/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import PlanListView, PlanAPIView
+
 urlpatterns = [
-    
+    path('', PlanListView.as_view()),
+    path('select/', PlanAPIView.as_view()),
 ]
--- Plan/views.py
+++ Plan/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import PlanSerializer
+from .models import Plan, Company
 # Create your views here.
+
+class PlanListView(GenericAPIView):
+    serializer_class = PlanSerializer
+
+    def get_queryset(self, company):
+        return Plan.objects.filter(company = company)
+    
+    def get(self, request):
+        company = request.query_params['company']
+        try:
+            company = Company.objects.get(name=company)
+        except :
+            return Response({"message" : "Invalid Company Selected"}, status=status.HTTP_400_BAD_REQUEST)
+        qs = self.get_queryset(company)
+        serializer = self.serializer_class(qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class PlanAPIView(GenericAPIView):
+    serializer_class = PlanSerializer
+
+    def get(self, request):
+        _id = request.query_params['id']
+        plan = Plan.objects.get(id = _id)
+        serializer = self.serializer_class(plan)
+        return Response(serializer.data, status=status.HTTP_200_OK)
